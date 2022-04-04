828fb914af608dc2bcc3829a2f2b19eec14a3bd8
Souza83 <tec.psouza@gmail.com>
2022-04-04 05:36:47 -0300

    Usando ListTile. Refatorando visualização das transações

--- lib/main.dart
+++ lib/main.dart
@@ -69,6 +69,18 @@ class _MyHomePageState extends State<MyHomePage> {
       value: 211.30,
       date: DateTime.now().subtract(Duration(days: 4)),
     ),
+    Transaction(
+      id: 't3',
+      title: "Cartao de Crédito",
+      value: 100211.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't4',
+      title: "Lanche",
+      value: 11.30,
+      date: DateTime.now(),
+    ),
   ];
 
   List<Transaction> get _recentTransactions {
