bc65672f2af6048920889deace6df30e48dfd645
Souza83 <tec.psouza@gmail.com>
2022-04-07 04:51:00 -0300

    Transação com Data.

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -3,7 +3,7 @@ import 'package:intl/intl.dart';
 
 class TransactionForm extends StatefulWidget {
   //Cria Função
-  final void Function(String, double) onSubmit;
+  final void Function(String, double, DateTime) onSubmit;
 
   //Cria Construtor
   TransactionForm(this.onSubmit);
@@ -21,11 +21,11 @@ class _TransactionFormState extends State<TransactionForm> {
     final title = _titleController.text;
     final value = double.tryParse(_valueController.text) ?? 0.0;
 
-    if (title.isEmpty || value <= 0) {
+    if (title.isEmpty || value <= 0 || _selectedDate == null) {
       //Se título em branco ou valor menor igual 0
       return;
     }
-    widget.onSubmit(title, value);
+    widget.onSubmit(title, value, _selectedDate);
   }
 
   //Cria Função privada para exibir calendário p/ selecionar data
